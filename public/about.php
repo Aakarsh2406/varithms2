7f6db6503b24c7507c971d49ce54461de20d841e
AakarshSingh2406 <46308570+Aakarsh2406@users.noreply.github.com>
2020-08-13 22:03:26 +0530

    Create about.php

---
+++ public/about.php
@@ -0,0 +1,68 @@
+  <!DOCTYPE html>
+<html>
+<head>
+<title></title>
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;500&display=swap" rel="stylesheet">
+  
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
+  <a class="navbar-brand" href="#">Varithms</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      
+  <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1>Varithms :)</h1>
+  <p>Learning Never Stops,share our website</p>
+</div
+<section class="my-5">
+<div class="py-5">
+<h2 class="text-center">About Us</h2>
+</div>
+<div class="container-fluid">
+<div class="row"> 
+	<div class="col-lg-6 col-md-6 col-12">
+<img src="images/A1.jpg" class="img-fluid aboutimg">
+</div>
+	<div class="col-lg-6 col-md-6 col-12">
+<h2 class="display-4">I am Varithms</h2>
+<p class="py-3"> Welcome to Varithms , a solution to all your algorithmic problems. 
+We are here dedicated to give you the best of knowledge keeping all your placement requirements in mind.
+
+Our main concern here is to make things a lot simpler to you. 
+Minimizing your struggle while understanding them.
+
+We're here to provide you with simpler techniques to understand particular algorithm and to implement it.
+For that we will be providing you with detailed explaination and implementation.</p>
+<a href="about.php" class="btn btn-success"> Know more </a>
+</div>
+</div>
+</div>
+</section>
+</body>
+</html>
