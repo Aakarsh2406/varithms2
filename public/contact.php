b705f464bd13f4715662e11bde831bda17272396
AakarshSingh2406 <46308570+Aakarsh2406@users.noreply.github.com>
2020-08-15 16:24:11 +0530

    Add files via upload

---
+++ public/contact.php
@@ -0,0 +1,81 @@
+  <!DOCTYPE html>
+<html>
+<head>
+<title></title>
+ <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;500&display=swap" rel="stylesheet">
+  
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
+  <a class="navbar-brand" href="#">Varithms</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      
+  <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+    <li class="breadcrumb-item"><a href="about.php">About</a></li>
+	<li class="breadcrumb-item"><a href="contact.php">Contact</a></li>
+  </ol>
+</nav>
+<div class="jumbotron">
+  <h1>Contact us</h1>
+  <p>All time here to help you peoples</p>
+</div
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-lg-4 col-md-4 col-12">
+<div class="card" style="width:300px">
+  <img class="card-img-top" src="images/A1.jpeg" alt="Card image" height="300vh">
+  <div class="card-body">
+    <h4 class="card-title">Aakarsh Singh</h4>
+    <p class="card-text">aakarshsingh2406@gmail.com</p>
+    <a href="www.linkedin.com/in/aakarsh-singh-2406" class="btn btn-success">Linkdin</a>
+</div>
+</div>
+</div>
+<div class="col-lg-4 col-md-4 col-12">
+<div class="card" style="width:300px">
+  <img class="card-img-top" src="images/V2.jpeg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Vivek Sharma</h4>
+    <p class="card-text">vivek.sharma7354@gmail.com</p>
+    <a href="https://www.linkedin.com/in/vstfiz" class="btn btn-success">Linkdin</a>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
