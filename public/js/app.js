293264d5aa3410f4c235fa427c26113df94da94d
vstfiz <vivek.sharma7354@gmail.com>
2020-08-17 20:03:10 +0530

    login module  complete

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+var uiConfig = {
+  signInSuccessUrl: 'index.html',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  
+  tosUrl: '<your-tos-url>',
+  privacyPolicyUrl: function() {
+    window.location.assign('<your-privacy-policy-url>');
+  }
+};
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+console.log("yahaan tak chal gayaa");
+ui.start('#sign-in-options', uiConfig);
