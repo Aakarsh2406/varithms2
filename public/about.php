007fe1f62de6d3e41341b9cc69731def85870567
Aakarsh Singh <akarsh.gangwar_cs18@gla.ac.in>
2020-08-22 19:07:22 +0530

    Update about.php

--- public/about.php
+++ public/about.php
@@ -6,8 +6,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="css/style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" >
   <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@200;500&display=swap" rel="stylesheet">
-  
+  <style> 
+
+</style>
 </head>
 <body>
 
@@ -27,18 +30,22 @@
       </li>
       
   <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
+        <a class="nav-link" href="contact.php">Contact</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+  
   </div>
 </nav>
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+    <li class="breadcrumb-item"><a href="about.php">About</a></li>
+	<li class="breadcrumb-item"><a href="contact.php">Contact</a></li>
+  </ol>
+</nav>
 <div class="jumbotron">
-  <h1>Varithms :)</h1>
-  <p>Learning Never Stops,share our website</p>
+  <h1 class="wow flipOutX">Varithms :)</h1>
+  <p class="wow zoomIn">Learning Never Stops,share our website</p>
 </div
 <section class="my-5">
 <div class="py-5">
@@ -47,7 +54,7 @@
 <div class="container-fluid">
 <div class="row"> 
 	<div class="col-lg-6 col-md-6 col-12">
-<img src="images/A1.jpg" class="img-fluid aboutimg">
+<img src="images/A7.jpg" class="img-fluid aboutimg">
 </div>
 	<div class="col-lg-6 col-md-6 col-12">
 <h2 class="display-4">I am Varithms</h2>
@@ -64,5 +71,31 @@ For that we will be providing you with detailed explaination and implementation.
 </div>
 </div>
 </section>
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.js" integrity="sha512-Rd5Gf5A6chsunOJte+gKWyECMqkG8MgBYD1u80LOOJBfl6ka9CtatRrD4P0P5Q5V/z/ecvOCSYC8tLoWNrCpPg==" crossorigin="anonymous"></script>
+<script>
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
+wow.init();
+</script>
+
+
+
 </body>
 </html>
