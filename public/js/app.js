da9adbf6b23533cb3b93d9542ea5fba23383ddaa
vstfiz <vivek.sharma7354@gmail.com>
2020-08-24 00:26:44 +0530

    signin options reduced

--- public/js/app.js
+++ public/js/app.js
@@ -4,11 +4,7 @@ var uiConfig = {
   signInOptions: [
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
   ],
   
   tosUrl: '<your-tos-url>',
